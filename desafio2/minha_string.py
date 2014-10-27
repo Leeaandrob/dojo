f67978e9024589a5307bbdc767bbad07d21500ea
Leeaandrob <leandrobarbosaa@outlook.com>
2014-10-27 13:37:19 -0200

    Finalizando o metodo upper_case e ajeitando os testes de upper_case

--- desafio2/minha_string.py
+++ desafio2/minha_string.py
@@ -1,6 +1,6 @@
 class String(object):
-    def __init__(self, nome):
-        self.nome = nome
+    def __init__(self, conteudo):
+        self.conteudo = conteudo
 
     def upper_case(self):
         conteudo = ""
@@ -11,11 +11,80 @@ class String(object):
             elif i == "b":
                 i = "B"
                 conteudo+=i
-            elif i == "C":
-                i == "c": 
-            
-         
-            
+            elif i == "c":
+                i = "C"
+                conteudo+=i
+            elif i == "d":
+                i = "D"
+                conteudo+=i
+            elif i == "e":
+                i = "E"
+                conteudo+=i
+            elif i == "f":
+                i = "F"
+                conteudo+=i
+            elif i == "g":
+                i = "G"
+                conteudo+=i
+            elif i == "h":
+                i = "H"
+                conteudo+=i
+            elif i == "i":
+                i = "i"
+                conteudo+=i
+            elif i == "j":
+                i = "J"
+                conteudo+=i
+            elif i == "k":
+                i = "K"
+                conteudo+=i
+            elif i == "l":
+                i = "L"
+                conteudo+=i
+            elif i == "m":
+                i = "M"
+                conteudo +=i
+            elif i == "n":
+                i = "N"
+                conteudo +=i
+            elif i == "o":
+                i = "O"
+                conteudo+=i
+            elif i == "p":
+                i = "P"
+                conteudo +=i
+            elif i == "q":
+                i = "Q"
+                conteudo +=i
+            elif i == "r":
+                i = "R"
+                conteudo+=i
+            elif i == "s":
+                i = "S"
+                conteudo+=i
+            elif i == "t":
+                i = "T"
+                conteudo+=i
+            elif i == "u":
+                i = "U"
+                conteudo+=i
+            elif i == "v":
+                i = "V"
+                conteudo+=i
+            elif i == "x":
+                i = "X"
+                conteudo+=i
+            elif i == "y":
+                i = "Y"
+                conteudo+=i
+            elif i == "w":
+                i="W"
+                conteudo+=i
+            elif i == "z":
+                i="Z"
+                conteudo+=i
+        self.conteudo = conteudo
+        return self.conteudo
             
             
             
--- desafio2/test_minha_string.py
+++ desafio2/test_minha_string.py
@@ -1,21 +1,12 @@
 from unittest import TestCase
-from minha_string import MinhaString
+from minha_string import String
 
 class StringTest(TestCase):
-   
-   def test_NewString(self):
-       
-       esperado = "Teste"
-       objeto = MinhaString(None)
-       resposta = objeto.NewString("Teste")
-
-       self.assertEqual(resposta, esperado)
-       
        
    def test_upper_case(self):
        
        esperado = "TESTE"
-       objeto = MinhaString("Teste")
+       objeto = String("teste")
        resposta = objeto.upper_case()
 
        self.assertEqual(resposta, esperado)
