0dfb274d56b26ea921d036f679e7ffedde611e56
Leeaandrob <leandrobarbosaa@outlook.com>
2014-11-02 18:19:28 -0200

    Corrigindo metodo upper_case()

--- desafio2/minha_string.py
+++ desafio2/minha_string.py
@@ -3,33 +3,36 @@ class String(object):
         self.conteudo = conteudo
         self.maisculo = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","Y","X","Z"]
         self.minusculo = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","y","x","z"]
+    
+    def indice(self, letra, lista):
+        indice=0
+        if not letra in lista:
+            return -1
+        for l in lista:
+            if l == letra:
+                return indice
+            else:
+                indice+=1
 
-    def upper_case(self):
-        letras = ""
-        contador = 0
-        for i in self.conteudo:
-            for j in self.minusculo:
-                if i != j :
-                    contador+=1
-                else:
-                    letras+=self.maisculo[contador]
-            contador=0
-            self.conteudo = letras
-        return self.conteudo
-
+    def upper_case(self): 
+        saida =  ""
+        for letra in self.conteudo:
+            indice = self.indice(letra, self.minusculo)
+            if indice != -1:
+                saida+=self.maisculo[indice]
+            else:
+                saida+=letra
+        return saida
     def lower_case(self):
-        letras = ""
-        contador = 0 
-        for i in self.conteudo:
-            for j in self.maisculo:
-                if i != j:
-                    contador+=1
-                else:
-                    letras+=self.minusculo[contador]
-            contador = 0
-            self.conteudo = letras
-        return self.conteudo
-                
+        saida = ""
+        for letras in self.conteudo:
+            indice = self.indice(letra, self.maisculo)
+            if indice != -1:
+                saida+=self.minusculo[indice]
+            else:
+                saida+=letras
+        return saida
+
     def tamanho(self):
         tamanho = 0 
         for i in self.conteudo:
@@ -42,13 +45,12 @@ class String(object):
          
     def contador(self, count):
         numero = 0
-        for i in self.conteudo:
-            if count in self.conteudo:
-                numero+=1
+        if count in self.conteudo:
+            numero+=1
         return numero
     
     def slice(self, comeco, fim, reposta):
-        if comeco > self.conteudo or fim:
+        if comeco > self.conteudo or self.conteudo > fim:
             reposta = False
         else:
             resposta = True
--- desafio2/test_minha_string.py
+++ desafio2/test_minha_string.py
@@ -1,25 +1,28 @@
 from unittest import TestCase
 from minha_string import String
-
+from unittest import skipIf
 class StringTest(TestCase):
        
    def test_upper_case(self):
-       
        esperado = "LEANDRO"
        objeto = String("leandro")
        resposta = objeto.upper_case()
 
        self.assertEqual(resposta, esperado) 
-   
-    
-   def test_tamanho(self):
+  
+   def test_upper_case_com_caracteres_especiais(self):
+       esperado = "T4ST4 MINHA STRING_@12345"
+       objeto = String("T4st4 minha string_@12345")
+       resposta = objeto.upper_case()
        
+       self.assertEqual(resposta, esperado)
+        
+   def test_tamanho(self): 
        esperado = 5
        objeto = String("Teste")
        resposta = objeto.tamanho()
        
        self.assertEqual(resposta, esperado)
-
 
    def test_lower_case(self):
        esperado = "teste"
@@ -36,7 +39,6 @@ class StringTest(TestCase):
         self.assertEqual(resposta, esperado) 
 
    def test_contador(self):
-        esperado = 3
+        esperado = 1
         objeto = String("teste")
-        resposta = objeto.contador("tes")
-        
+        resposta = objeto.contador("tes")      
