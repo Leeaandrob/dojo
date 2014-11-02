138a54d651e849279e523551fe3638c825beaa4f
Leeaandrob <leandrobarbosaa@outlook.com>
2014-11-02 18:24:35 -0200

    Adicionando test_lower_case_com_caractares_especiais

--- desafio2/test_minha_string.py
+++ desafio2/test_minha_string.py
@@ -30,6 +30,13 @@ class StringTest(TestCase):
        resposta = objeto.lower_case()
        
        self.assertEqual(resposta, esperado)
+   
+   def test_lower_case_com_caracteres_especiais(self):
+       esperado = "t4st4 @123456"
+       objeto = String("T4sT4 @123456")
+       resposta = objeto.lower_case()
+
+       self.assertEqual(resposta, esperado)
        
    def test_centralizar(self):
         esperado = "aaaatesteaaaa"
